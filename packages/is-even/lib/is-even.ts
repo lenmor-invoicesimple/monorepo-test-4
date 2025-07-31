ccdc1ddc3af6b30bbb768160746565cd1b672d3d
Lenmor Dimanalata <lenmor@invoicesimple.com>
2025-07-31 14:15:24 -0400

    commit package changes

--- packages/is-even/lib/is-even.ts
+++ packages/is-even/lib/is-even.ts
@@ -1,6 +1,6 @@
 import { isOdd } from "@lenmor-invoicesimple/is-odd";
 
 export function isEven(num: number): boolean {
-  console.log("isEven called with 111:", num);
+  console.log("isEven called with 11122");
   return !isOdd(num);
 }
